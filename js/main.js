0c69f95159b09950f8669bc2844d2482acd10bfb
designlab75 <yesme391@gmail.com>
2020-03-09 21:37:32 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,212 @@
+(function ($) {
+"use strict";
+
+// meanmenu
+jQuery(document).ready(function () {
+        jQuery('.menu').meanmenu();
+    });
+
+// START MENU JS CODE
+		$(window).on('scroll', function() {
+			if ($(this).scrollTop() > 100) {
+				$('.main-menu').addClass('menu-shrink animated slideInDown');
+			} else {
+				$('.main-menu').removeClass('menu-shrink animated slideInUp');
+			}
+		});	
+		
+		$('.menu li a').on('click', function(e){
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top - 50
+			}, 1000);
+			e.preventDefault();
+        });
+		
+		$(document).on('click','.navbar-collapse.in',function(e) {
+			if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+				$(this).collapse('hide');
+			}
+        });
+
+
+
+// One Page Nav
+var top_offset = $('.header-area').height() - 10;
+$('.main-menu nav ul').onePageNav({
+	currentClass: 'active',
+	scrollOffset: top_offset,
+});
+
+
+$(window).on('scroll', function () {
+	var scroll = $(window).scrollTop();
+	if (scroll < 245) {
+		$(".header-sticky").removeClass("sticky");
+	} else {
+		$(".header-sticky").addClass("sticky");
+	}
+});
+
+
+
+// mainSlider
+function mainSlider() {
+	var BasicSlider = $('.slider-active');
+	BasicSlider.on('init', function (e, slick) {
+		var $firstAnimatingElements = $('.single-slide:first-child').find('[data-animation]');
+		doAnimations($firstAnimatingElements);
+	});
+	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+		doAnimations($animatingElements);
+	});
+	BasicSlider.slick({
+		autoplay: false,
+		autoplaySpeed: 10000,
+		dots: true,
+		fade: true,
+		arrows: false,
+		responsive: [
+			{ breakpoint: 767, settings: { dots: false, arrows: false } }
+		]
+	});
+
+	function doAnimations(elements) {
+		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		elements.each(function () {
+			var $this = $(this);
+			var $animationDelay = $this.data('delay');
+			var $animationType = 'animated ' + $this.data('animation');
+			$this.css({
+				'animation-delay': $animationDelay,
+				'-webkit-animation-delay': $animationDelay
+			});
+			$this.addClass($animationType).one(animationEndEvents, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+}
+mainSlider();
+
+
+// owlCarousel
+$('.header-slide').owlCarousel({
+    loop:true,
+    margin:0,
+	items:1,
+	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    nav:false,
+	dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:1
+        },
+        992:{
+            items:1
+        }
+    }
+})
+
+// portfolio slider-active
+// owlCarousel
+$('.customer-logos').owlCarousel({
+    loop:true,
+    margin:0,
+	items:1,
+	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    nav:false,
+	dots:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        767:{
+            items:4
+        },
+        992:{
+            items:5
+        }
+    }
+})
+
+
+/* magnificPopup img view */
+$('.popup-image').magnificPopup({
+	type: 'image',
+	gallery: {
+	  enabled: true
+	}
+});
+
+/* magnificPopup video view */
+$('.popup-video').magnificPopup({
+	type: 'iframe'
+});
+
+
+// isotop
+$('.grid').imagesLoaded( function() {
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  itemSelector: '.grid-item',
+	  percentPosition: true,
+	  masonry: {
+		// use outer width of grid-sizer for columnWidth
+		columnWidth: '.grid-item',
+	  }
+	});
+});
+
+// filter items on button click
+$('.portfolio-menu').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+});
+
+//for menu active class
+$('.portfolio-menu button').on('click', function(event) {
+	$(this).siblings('.active').removeClass('active');
+	$(this).addClass('active');
+	event.preventDefault();
+});
+
+
+
+
+// scrollToTop
+$.scrollUp({
+	scrollName: 'scrollUp', // Element ID
+	topDistance: '300', // Distance from top before showing element (px)
+	topSpeed: 300, // Speed back to top (ms)
+	animation: 'fade', // Fade, slide, none
+	animationInSpeed: 200, // Animation in speed (ms)
+	animationOutSpeed: 200, // Animation out speed (ms)
+	scrollText: '<i class="icofont icofont-long-arrow-up"></i>', // Text for element
+	activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+});
+
+// WOW active
+new WOW().init();
+
+
+// Search Area Js
+
+
+
+$("#serch-icon").click(function(){
+    $(".search-box").addClass("show");
+  });
+
+
+$(".close-search").click(function(){
+    $(".search-box").removeClass("show");
+  });
+
+
+
+})(jQuery);
